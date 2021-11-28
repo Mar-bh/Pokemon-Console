e22b15e36d3536032f5668039992eae9bdbdf9cc
Mariana Esquivel <esquivelmariana0702@gmail.com>
2021-11-27 19:45:54 -0600

    Ay

--- Pokedex.cpp
+++ Pokedex.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-<<<<<<< HEAD
  using namespace std;
 
  int main() {
      cout << "Mariana change";
         
  }
-=======
-
-using namespace std;
-
-int main() {
-
-    cout << "hoal mundo" << endl;
-    return 0;
-}
->>>>>>> 63ce6915f720d102f050cc6da3575755d5a6056a
