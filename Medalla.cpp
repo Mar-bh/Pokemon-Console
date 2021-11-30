18d2f5a87b05fb8544c2ad2674620375e345ffa4
Mariana <marianabhdez@gmail.com>
2021-11-29 19:33:04 -0600

    medalla

--- Medalla.cpp
+++ Medalla.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include "Medalla.h"
 #include <string>
+
+Medalla::Medalla(){
+    nombre = "Sin medalla";
+    valor = "Sin valor";
+}
+
+Medalla::Medalla(std::string _nombre, std::string _valor){
+    nombre = _nombre;
+    valor = _valor;
+}
+
+std::string Medalla::getNombre(){
+    return nombre;
+}
+
+std::
+
