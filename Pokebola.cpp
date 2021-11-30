d2d28dbc3a50b57445d663f33c4718c10751dd1a
Mariana <marianabhdez@gmail.com>
2021-11-29 20:06:17 -0600

    avance pokebola

--- Pokebola.cpp
+++ Pokebola.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include "Pokebola.h"
 #include <string>
+
+Pokebola::Pokebola(){
+    tipo = "Pokebola";
+    pokemon = Pokemon();
+}
+
+Pokebola::Pokebola(std::string _tipo, Pokemon _pokemon){
+    tipo = _tipo;
+    pokemon = _pokemon;
+}
