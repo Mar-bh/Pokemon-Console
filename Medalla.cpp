e0fb1e47a34c557e0fe7332c2e58f1b5cfa7ba62
Mariana <marianabhdez@gmail.com>
2021-11-29 19:48:28 -0600

    Fin medalla cpp

--- Medalla.cpp
+++ Medalla.cpp
@@ -12,9 +12,26 @@ Medalla::Medalla(std::string _nombre, std::string _valor){
     valor = _valor;
 }
 
-std::string Medalla::getNombre() const;
+std::string Medalla::getNombre() const{
     return nombre;
 }
 
-std::
+std::string Medalla::getValor() const{
+    return valor;
+}
+
+void Medalla::setNombre(std::string _nombre){
+    nombre = _nombre;
+}
+
+void Medalla::setValor(std::string _valor){
+    valor = _valor;
+}
+
+void Medalla::imprime(){
+    std::cout << "--------------MEDALLA----------------" << std::endl;
+    std::cout<< "El nombre es: " << nombre << std::endl; 
+    std::cout<< "El valor es: " << valor << std::endl; 
+   
+}
 
