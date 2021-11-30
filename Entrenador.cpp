96478c238e0af705ffc17139bf1b04e19f5b9189
Mariana Esquivel <esquivelmariana0702@gmail.com>
2021-11-29 18:26:24 -0600

    listo?

--- Entrenador.cpp
+++ Entrenador.cpp
@@ -15,39 +15,15 @@ Entrenador::Entrenador(std::string _nomb, int _experiencia, int _meda) {
 }
 
 //Metodos de acceso o getters
-double Entrenador::getnombre() const {
+std::string Entrenador::getnombre() const {
 	return nombre;
 
 }
-double Entrenador::getexperiencia() const {
+int Entrenador::getexperiencia() const {
 	return experiencia;
 
 }
-double Entrenador::getmedallas() const {
-	return medalla;
+int Entrenador::getmedallas() const {
+	return medallas;
 
-}
-
-//Metodos modificadores o setters
-void Punto::setx(double _valx) {
-	x = _valx;
-}
-void Punto::sety(double _valy) {
-	y = _valy;
-}
-
-//Metodos adicionales
-double Punto::distance(Punto P) {
-	double dist = sqrt(pow(P.getx() - x, 2) + pow(P.gety() - y, 2));
-	return dist;
-}
-
-std::string Punto::toStringPunto() {
-	std::string toPrint = "(" + std::to_string(x) + "," + std::to_string(y) + ")";
-	return toPrint;
-
-}
-
-void Punto::imprime() {
-	std::cout << "x: " << x << " y: " << y << std::endl;
 }
