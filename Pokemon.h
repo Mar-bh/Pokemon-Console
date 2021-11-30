cd70b840e5ee80a1157387c7bef25be0030fbdd8
Mariana <marianabhdez@gmail.com>
2021-11-29 18:54:51 -0600

    pokemon clase

--- Pokemon.h
+++ Pokemon.h
@@ -7,9 +7,23 @@ class Pokemon {
     public:
         Pokemon();
         Pokemon(std::string,std::string,int,std::string[]);
-        std::string
+        std::string getNombre() const;
+        std::string getTipo() const;
+        int getNivel() const;
+        std::string[] getHabilidades() const;
+        void setNombre(std::string);
+        void setTipo(std::string);
+        void setNivel(std::string);
+        void setHabilidades(std::string[]);
+        void imprime();
+        void subirNivel();
+        void utilizarHabilidad(int);
         
     private:
+    std::string nombre;
+    std::string tipo;
+    int nivel;
+    std::string[] habilidades;
         
 };
 
