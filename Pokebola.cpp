daf5b3959084890f440c1a3adddcb248ac7fb99a
Mariana <marianabhdez@gmail.com>
2021-11-30 18:00:21 -0600

    Cambios

--- Pokebola.cpp
+++ Pokebola.cpp
@@ -11,3 +11,32 @@ Pokebola::Pokebola(std::string _tipo, Pokemon _pokemon){
     tipo = _tipo;
     pokemon = _pokemon;
 }
+
+std::string Pokebola::getTipo() const{
+    return tipo;
+}
+
+Pokemon Pokebola::getPokemon() const{
+    return pokemon;
+}
+
+void Pokebola::setTipo(std::string){
+    
+
+}
+
+void Pokebola::setPokemon(Pokemon){
+
+}
+
+void Pokebola::atrapar(bool){
+
+}
+
+void Pokebola::liberar(bool){
+    
+}
+
+void imprime(){
+
+}
