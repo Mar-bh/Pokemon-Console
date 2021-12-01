4948253851e3c5bd71fdf5edd5e452406c9b471b
Mariana <marianabhdez@gmail.com>
2021-12-01 13:29:57 -0600

    agregar namespace

--- mainPokedex.cpp
+++ mainPokedex.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
 #include "Entrenador.h"
-#include "Pokemon.h"
-#include "Medalla.h"
 #include "Pokebola.h"
 #include <string>
+using namespace std;
 
 
  int main() {
     int opcion = 0;
     int validacionPokemon = 0;
-    std::string nombre;
+    string nombre;
     int experiencia;
     int medallas;
-    std::string rango;
-    std::string nombrePoke;
-    std::string tipo;
+    string rango;
+    string nombrePoke;
+    string tipo;
     int nivel;
-    std::string habilidades;
-    std::string rango;
-    std::cout << "Write your Champion name: ";
-	std::cin >> nombre;
-	std::cout << "Write your years of experience";
-	std::cin >> experiencia;
-	std::cout << "Write the quantoty of medals you have";
-	std::cin >> medallas; 
-    std::cout << "Do you want to use your pokemon?\n1) Yes :D\n2) No >:(";
-    std::cin >> validacionPokemon;
+    string habilidades;
+    string rango;
+    cout << "Write your Champion name: ";
+	cin >> nombre;
+	cout << "Write your years of experience";
+	cin >> experiencia;
+	cout << "Write the quantoty of medals you have";
+	cin >> medallas; 
+    cout << "Do you want to use your pokemon?\n1) Yes :D\n2) No >:(";
+    cin >> validacionPokemon;
     if (validacionPokemon == 1){
-        std::cout << "Wich hability you want to use:\n1)Punch\n2)Special Hability\n3)Acumulated Power\nOverkill";
-        std::cin >> opcion; 
+         cout << "Wich hability you want to use:\n1)Punch\n2)Special Hability\n3)Acumulated Power\nOverkill";
+         cin >> opcion; 
     }
 
     Entrenador entrenador(nombre, experiencia, medallas,rango);
